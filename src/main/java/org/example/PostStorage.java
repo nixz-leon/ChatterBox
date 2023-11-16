88790186ee3e4879bf7b56d9e5cd409810c222aa
Leon <nixzleon@gmail.com>
2023-11-15 18:43:26 -0700

    Added in the storing to csv file

--- src/main/java/org/example/PostStorage.java
+++ src/main/java/org/example/PostStorage.java
@@ -2,8 +2,12 @@ package org.example;
 
 import org.example.Post;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.FileReader;
+import java.io.File;
+import java.io.FileWriter;
 
 public class PostStorage {
     private List<Post> posts;
@@ -18,5 +22,23 @@ public class PostStorage {
 
     public List<Post> getAllPosts() {
         return posts;
+    }
+
+    public void storePosts(String filename) throws IOException {
+        File csvFile = new File(filename);
+        FileWriter filewriter = new FileWriter(csvFile);
+        for (Post post : posts) {
+            String postInfo = post.getContent();
+            String postId = post.getId();
+            String postAuthor = post.getAuthor();
+            String postTimestamp = post.getTimestamp().toString();
+            String line = postInfo + ',' + postId + ',' + postAuthor +','+postTimestamp+"\n";
+            filewriter.write(line);
+        }
+        filewriter.close();
+    }
+
+    public void loadPosts(String filename){
+
     }
 }
